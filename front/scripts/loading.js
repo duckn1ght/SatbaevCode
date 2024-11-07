3a32027b258827d6d9f81c3d0ec490a170a56e65
duckn1ght <duckn1ght04@gmail.com>
2024-11-08 00:32:22 +0500

    loading page fix

--- front/scripts/loading.js
+++ front/scripts/loading.js
@@ -1,13 +1,13 @@
 (function loadinfAnimation(){
-    // let textBox = document.getElementById("loadingText");
-    // setInterval(()=>{
-    //     textBox.textContent = "Оценка эмоций...";
-    //     setInterval(()=>{
-    //         textBox.textContent = "Завершаем...";
-    //         setInterval(()=>{
-    //             // window.location.href = "index.html"\
-    //             console.log(1)
-    //         },2000)
-    //     },1400)
-    // },2300);
+    let textBox = document.getElementById("loadingText");
+    setInterval(()=>{
+        textBox.textContent = "Оценка эмоций...";
+        setInterval(()=>{
+            textBox.textContent = "Завершаем...";
+            setInterval(()=>{
+                window.location.href = "result.html"
+                console.log(1)
+            },2000)
+        },1400)
+    },2300);
 }());
