d6b0edda53a13f0ec0b96e1a05c00a6e5cd6aea3
Vasilyev Maksim <3103maksim96@gmail.com>
2024-11-08 01:45:47 +0500

    поменял CSS в резалт

--- front/scripts/results.js
+++ front/scripts/results.js
@@ -1,3 +1,3 @@
 let imageUrl = localStorage.getItem('imageBase64')
 let userInfo = localStorage.getItem('info')
-document.getElementById('resultImg').src = imageUrl;
+document.getElementById('resultImg').src = "src/1612298572_91-p-anime-peizazh-fon-fioletovii-138.png";
